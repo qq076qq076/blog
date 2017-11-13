2937e3a82644d8c4a58fb7a4b6b321f14c9842de
qq076qq076 <qq076qq076@gamil.com>
2017-11-13 17:56:07 +0800

    調整header浮動方式

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +9,12 @@ export class HeaderComponent implements OnInit {
   nav_list: Array<any>;
   need_fixed = false;
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    // console.log(number);
-    this.need_fixed = (number > 60);
-  }
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  //   // console.log(number);
+  //   this.need_fixed = (number > 60);
+  // }
   constructor() {}
 
   ngOnInit() {
